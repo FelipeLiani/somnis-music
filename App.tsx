7743476420399ba3ba1c5a66c0e8617f77cca6c5
fliani <104031747+FelipeLiani@users.noreply.github.com>
2024-07-06 23:23:27 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.view1}>
+      <View style={styles.view2}>
+        <Text>App</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  view1: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  view2: {
+    height:100,
+    width:100,
+    backgroundColor:'red',
+    justifyContent:'center',
+    alignItems:'center'
+  }
+})
